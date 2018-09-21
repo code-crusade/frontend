c3a8a5526d9d18081ea92bf653fc8053880255fa
Skander <skander.kc@gmail.com>
2018-09-21 17:12:23 -0400

    Basic redux store setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { App } from './App';
+import { store } from './store';
+
+interface RootProps {
+  store: any;
+}
+
+class Root extends React.PureComponent<RootProps> {
+  render() {
+    return(
+      <Provider store={this.props.store}>
+        <App />
+      </Provider>
+    );
+  }
+}
 
 ReactDOM.render(
-  <App />,
+  <Root store={store} />,
   document.getElementById('root')
 );
