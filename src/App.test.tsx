d6b7e1c779c617653f82fcbd08acb22dcfd557bd
Skander <skander.kc@gmail.com>
2018-09-24 00:16:54 -0400

    Attempt to fix test with history

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
+import { createMemoryHistory } from 'history'
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import configureStore from 'redux-mock-store';
 import { App } from './App';
-import { history } from './store';
 
 const mockStore = configureStore();
+const history = createMemoryHistory();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ConnectedRouter } from 'connected-react-router'
+import { ConnectedRouter } from 'connected-react-router';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { Routes } from './routes';
