8b38f7636df5e903088294876554b88532b8515a
Skander <skander.kc@gmail.com>
2018-09-26 19:40:53 -0400

    Import blueprint

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import * as ReactDOM from 'react-dom';
 import { App } from './App';
 import { history, store } from './store';
 
+// Import styling
+import 'normalize.css/normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css'; // tslint:disable-line
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+
 ReactDOM.render(
   <App store={store} history={history} />,
   document.getElementById('root'),
