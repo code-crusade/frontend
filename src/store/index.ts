3bc5dae588783b3c4d4ad60fb3342d1b4dbf16c2
Skander <skander.kc@gmail.com>
2018-09-26 18:31:17 -0400

    Add redux-logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,19 +2,25 @@ import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
 import * as services from '../services';
 import { rootEpic } from './root-epic';
 import { rootReducer } from './root-reducer';
 
 export const history = createBrowserHistory();
+// Configure redux-logger
+// https://www.npmjs.com/package/redux-logger#options
+const logger = createLogger({
+  collapsed: true,
+});
 const epicMiddleware = createEpicMiddleware({
   // Dependencies are accessed as the third parameter when declaring an epic
   dependencies: services,
 });
 
 function configureStore(initialState?: {}) {
-  const middlewares = [epicMiddleware, routerMiddleware(history)];
+  const middlewares = [logger, epicMiddleware, routerMiddleware(history)];
   const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
 
   const newStore = createStore(
